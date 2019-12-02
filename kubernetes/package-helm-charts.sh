daec64395495134ba4553d9f9b766408432ee4be
Michel <mnemonic01@users.noreply.github.com>
2019-12-02 09:29:34 +0100

    Update package-helm-charts.sh

--- kubernetes/package-helm-charts.sh
+++ kubernetes/package-helm-charts.sh
@@ -6,29 +6,22 @@
 
 HELM_URL=https://get.helm.sh
 HELM_TARBALL=helm-v2.7.2-linux-amd64.tar.gz
-HELM_EXTRACTED_ARCHIVE="$(pwd)/linux-amd64/"
+HELM_EXTRACTED_ARCHIVE="$SCRIPTPATH/charts/"
+SCRIPTPATH="$( cd "$(dirname "$0")" > /dev/null 2>&1 ; pwd -P )"
+WOCHARTPATH="$SCRIPTPATH/charts/weblogic-operator"
+
 
 install_helm () {
 
   # Download and install helm
-  curl -o "${HELM_TARBALL}" "${HELM_URL}/${HELM_TARBALL}"
-  tar zxvf ${HELM_TARBALL}
-  PATH=${PATH}:${HELM_EXTRACTED_ARCHIVE}
-  export PATH
-  rm -f ${HELM_TARBALL}
-
-  # Install helm s3 plugin if not installed
-  if [[ $(helm plugin list | grep "^s3") == "" ]]; then
-    helm plugin install https://github.com/hypnoglow/helm-s3.git
-  fi
+curl -o "${HELM_TARBALL}" "${HELM_URL}/${HELM_TARBALL}"
+tar zxvf ${HELM_TARBALL}
+PATH=${PATH}:${HELM_EXTRACTED_ARCHIVE}
+export PATH
+rm -f ${HELM_TARBALL}
 }
-
 install_helm
 
-
-
-SCRIPTPATH="$( cd "$(dirname "$0")" > /dev/null 2>&1 ; pwd -P )"
-WOCHARTPATH="$SCRIPTPATH/charts/weblogic-operator"
 
 if uname | grep -q "Darwin"; then
     mod_time_fmt="-f %m"
